e41a37dbed48200232b94bae3cf49ad8b46bb127
ltpthao <ltpthao@tma.com.vn>
2023-01-17 17:12:52 +0700

    Build Header #6

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -24,6 +24,13 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                { code: 'en', title: 'English' },
+                { code: 'vi', title: 'Tiếng Việt' },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -43,6 +50,11 @@ function Header() {
             setSearchResult([]);
         }, 100);
     }, []);
+
+    //Handle logic
+    const handleMenuChange = (menuItem) => {
+        console.log(menuItem);
+    };
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -78,7 +90,7 @@ function Header() {
                 <div className={cx('actions')}>
                     <Button text>Upload</Button>
                     <Button primary>Log in</Button>
-                    <Menu items={MENU_ITEMS}>
+                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                         <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
                         </button>
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,15 +1,36 @@
 import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
+import { useState } from 'react';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
+import Header from './Header';
 import styles from './Menu.module.scss';
 import MenuItem from './MenuItem';
 
 const cx = classNames.bind(styles);
-
-function Menu({ children, items = [] }) {
+const defaultFn = () => {};
+function Menu({ children, items = [], onChange = defaultFn }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
     const renderItems = () => {
-        return items.map((item, index) => <MenuItem key={index} data={item} />);
+        return current.data.map((item, index) => {
+            const isParent = !!item.children; //!!conver boolen
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((pre) => [...history, item.children]);
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            );
+        });
     };
+    //onsole.log('history', history);
+    //console.log('history.lenght', history.length);
     return (
         <Tippy
             interactive
@@ -17,7 +38,17 @@ function Menu({ children, items = [] }) {
             placement="bottom-end"
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx('menu-poper')}>{renderItems()}</PopperWrapper>
+                    <PopperWrapper className={cx('menu-poper')}>
+                        {history.length > 1 && (
+                            <Header
+                                title="Language"
+                                onBack={() => {
+                                    setHistory((pre) => pre.slice(0, pre.length - 1)); //cắt phần tử cuối
+                                }}
+                            />
+                        )}
+                        {renderItems()}
+                    </PopperWrapper>
                 </div>
             )}
         >
